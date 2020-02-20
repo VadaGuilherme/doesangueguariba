40335724f59b7f2c304164979163c0ab40faea59
Guilherme Vada <gvadaguariba@gmail.com>
2020-02-20 00:12:39 -0300

    Integracao com o db

--- server.js
+++ server.js
@@ -6,32 +6,29 @@ server.use(express.static('public'))
 
 server.use(express.urlencoded({ extended: true }));
 
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '',
+    host: '',
+    port: 5432,
+    database: 'doe'
+})
+
 nunjuks.configure("./", {
     express: server,
     noCache: true,
 })
 
-const donors = [
-    {
-        name: "1",
-        blood: "1",
-    },
-    {
-        name: "2",
-        blood: "2",
-    },
-    {
-        name: "3",
-        blood: "3",
-    },
-    {
-        name: "4",
-        blood: "4",
-    },
-]
-
 server.get("/", function(req, res) {
-    return res.render("index.html", { donors })
+    const query = "SELECT * FROM donors"
+
+    db.query(query, function(err, result){
+        if(err) return res.send("Erro ao listar donors.")
+
+        const donors = result.rows
+        return res.render("index.html", { donors })
+    })
 })
 
 server.post("/", function(req, res){
@@ -39,12 +36,21 @@ server.post("/", function(req, res){
     const blood = req.body.blood
     const cellphone = req.body.cellphone
 
-    donors.push({
-        name: name,
-        blood: blood,
-    })
+    if(name == "" || cellphone == "" || blood == "") {
+        return res.send("Todos os campos sao obrigatorios.")
+    }
 
-    return res.redirect("/")
+    const query = `
+        INSERT INTO donors ("name", "cellphone", "blood") 
+        VALUES ($1, $2, $3)`
+
+    const values = [name, cellphone, blood]
+
+    db.query(query, values, function(err) {
+        if(err) return res.send("Erro ao cadastrar donors.")
+
+        return res.redirect("/")
+    })
 })
 
 server.listen(3000, function() {
